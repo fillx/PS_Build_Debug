66431f27448cb319977d09bc7061ae7e7580fc0b
Filipp <f.ivashov@gmail.com>
2024-09-20 11:41:48 +0300

    1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Hexacore-Pocket Space-0.1.4";
+const contentToCache = [
+    "Build/PS_Build_Debug.loader.js",
+    "Build/PS_Build_Debug.framework.js.unityweb",
+    "Build/PS_Build_Debug.data.unityweb",
+    "Build/PS_Build_Debug.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
